838e76e97a8b32546e9fa847934e4e0131ef1b6d
ManasKapoor18 <manasmk2016@gmail.com>
2023-11-17 20:41:42 +0530

    added all files of blog app

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb+srv://manasmk2016:1234@cluster0.cn54ogo.mongodb.net/?retryWrites=true&w=majority",
+  cookieKey: "123123123",
+};
+                        
